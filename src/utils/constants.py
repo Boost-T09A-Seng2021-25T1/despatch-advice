388b412751b1d6ea048f4b59f049d87264eafb09
Ched Toriano <nimble2412@users.noreply.github.com>
2025-03-09 13:39:04 +1100

    fix: cleaned up the repo, added files under src and started the conversion function
    - cleaned up the repo,
    - added files under src
    - started the conversion function

---
+++ src/utils/constants.py
@@ -0,0 +1,11 @@
+# ================================================
+# This file will store namespace variables
+
+# ================================================
+
+# some namespaces are constant for simplicity for now,
+#  might need to add customization here later
+
+cacSchema = 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'
+cbcSchema = 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'
+despatchSchema = 'urn:oasis:names:specification:ubl:schema:xsd:DespatchAdvice-2'
---
+++ src/xmlConversion.py
@@ -0,0 +1,15 @@
+# ================================================
+# This file will store functions related to 
+# creating the final despatch advice.
+
+# ================================================
+
+from lxml import etree
+from utils.constants import cacSchema, cbcSchema, despatchSchema
+
+def xmlConvert():
+
+    xmlns = { 'cac': cacSchema, 'cbc': cbcSchema }
+    root = etree.Element(f"{despatchSchema}")
+
+    # etree.SubElement()
