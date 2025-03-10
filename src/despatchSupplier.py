f328e086c28080398d2d00168157a876574fc672
Ched Toriano <nimble2412@users.noreply.github.com>
2025-03-11 10:36:03 +1100

    feat: added first draft for despatch supplier party
    - used unittest and added a dummy file to reference

---
+++ src/despatchSupplier.py
@@ -0,0 +1,3 @@
+
+def despatchSupplier():
+    return True
---
+++ tests/testDespatchSupplier.py
@@ -0,0 +1,24 @@
+import unittest
+from src.despatchSupplier import despatchSupplier
+
+# Testing the despatch supplier section
+class testDespatchSupplier(unittest.Testcase):
+
+    # test that the output contains all req subsection
+    def testCorrectProperties(self):
+        expectedOuterKeys = { "CustomerAssignedAccountID", "Party", }
+        expectedPartyKeys = {
+            "PartyName",
+            "PostalAddress",
+            "PartyTaxScheme",
+            "Contact"
+        }
+
+        functionOutput = despatchSupplier()
+
+        # test for correct outer objects and properties
+        self.assertDictEqual(
+            functionOutput, 
+            expectedOuterKeys, 
+            "Error: incorrect dict properties"
+        )
