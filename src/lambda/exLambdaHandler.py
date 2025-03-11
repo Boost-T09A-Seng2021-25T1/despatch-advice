0a83ef09e500ec53c08159d1b80d8ae113bc52aa
Ched Toriano <nimble2412@users.noreply.github.com>
2025-03-12 02:14:10 +1100

    feat: completed testing and implementation of mongodb
    - added test suite for mongodb in tests folder
    - added .env file with URI from mongodb
    - added mongodb implementation
    - all tests passing
    - added __init__.py file

--- src/lambda/exLambdaHandler.py
+++ src/lambda/exLambdaHandler.py
@@ -2,6 +2,7 @@ import json
 from src.mongodb import collection
 from src.utils.constants import STATUS_SUCCESS, STATUS_BAD_REQ
 
+
 # this handles AWS Gateway API reqs
 def handler(event):
 
@@ -9,7 +10,7 @@ def handler(event):
     if event["httpMethod"] == "POST":
         body = json.loads(event["body"])
 
-        #save data
+        # save data
         collection.insert(body)
 
         return {
@@ -20,20 +21,20 @@ def handler(event):
                 }
             )
         }
-    
+
     # draft get method
     elif event["httpMethod"] == "GET":
         data = list(collection.find(
             {},
             # example, except id.
-            { "_id": 0 }
+            {"_id": 0}
         ))
 
         return {
             "statusCode": STATUS_SUCCESS,
             "body": json.dumps(data)
         }
-    
+
     # draft everything else to be implemented
     return {
         "statusCode": STATUS_BAD_REQ,
--- src/mongodb.py
+++ src/mongodb.py
@@ -1,18 +1,79 @@
 from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
+from pymongo.server_api import ServerApi
 import os
 
-load_dotenv()
-uri = os.getenv("MDB_URI")
-client = MongoClient(uri)
 
-# Connection test
+load_dotenv(
+    dotenv_path=os.path.join(
+        os.path.dirname(__file__),
+        "../config/.env"
+    )
+)
+
+uri = os.getenv("MDB_URI")
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# name of the mongodb database
+db = client["ubl_docs"]
+# name of the collection inside the mongodb
+orders = db["orders"]
+
+# Connection test on startup
 try:
     client.admin.command('ping')
     print("Successfully connected to MongoDB!")
 
-    db = client["database"]
-    collection = db["collection"]
-
 except Exception as error:
-    print(error)
+    print(f"Connection failed: {error}")
+
+
+# ===========================================
+# Purpose: Database function to import.
+# Add order to db.The data MUST HAVE A UUID STRING KEY
+
+# Argument: object, preferably in the order ubl structure
+
+# Return: added order ID
+# ============================================
+
+
+def addOrder(data):
+    try:
+        return orders.insert_one(data).inserted_id
+
+    except Exception as error:
+        print(f"MongoDB request failed: {error}")
+
+
+# ===========================================
+# Purpose: Database function to import. Fetch order doc.
+# Argument: order UUID string
+
+# Return: fetched order object
+# ============================================
+
+
+def getOrderInfo(orderUUID):
+    try:
+        return orders.find_one({"UUID": orderUUID})
+
+    except Exception as error:
+        print(f"MongoDB fetch failed: {error}")
+
+
+# ===========================================
+# Purpose: Database function to import. Deletes order
+# Argument: order UUID string
+
+# Return: true if item deleted
+# ============================================
+
+
+def deleteOrder(orderUUID):
+    try:
+        orders.delete_one({"UUID": orderUUID})
+        return True
+
+    except Exception as error:
+        print(f"MongoDB delete failed: {error}")
