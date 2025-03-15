72bc033d588a7bdb5c530e82d3277afdab55f8b1
Ched Toriano <nimble2412@users.noreply.github.com>
2025-03-15 22:10:14 +1100

    style: fixed linting issues

--- src/apiEndpoint.py
+++ src/apiEndpoint.py
@@ -6,19 +6,18 @@ def endpointFunc(
     xmlDoc: str,
     shipment: dict,
     despatch: dict,
-    supplier: dict):
+    supplier: dict
+):
 
     if xmlDoc is None or not isinstance(xmlDoc, str):
         raise TypeError("Error: document is invalid.")
 
     if any(key is None or not isinstance(key, dict) for key in [
             shipment, despatch, supplier
-    ]): 
+    ]):
         raise TypeError(
             "Error: invalid shipment or despatch information"
         )
-
-
 
     # call Arnav's func here for xml to json
 
