61cd4c744e793a14d410c87c448281410c800a3c
Ched Toriano <nimble2412@users.noreply.github.com>
2025-04-22 07:58:32 +1000

    chore: added createDespatch page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,6 +30,7 @@ const App = () => {
                 <Route path="/" element={<Index />} />
                 <Route path="/signin" element={<SignIn setUser={setUser} />} />
                 <Route path="/dashboard" element={<Dashboard user={user} />} />
+                <Route path="/create-despatch" element={<CreateDespatch />} />
                 <Route path="/logged-out" element={<LoggedOut />} />
               </Routes>
             </BrowserRouter>
