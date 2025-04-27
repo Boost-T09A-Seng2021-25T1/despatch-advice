31ed1db6fd33a77488391f63b83a0a4396dab6cb
Ched Toriano <nimble2412@users.noreply.github.com>
2025-04-27 21:46:54 +1000

    Revert "chore: added email function in createdespatch"
    This reverts commit 15e0acba17985cf1614399826298d128abfae721.

--- frontend/src/pages/CreateDespatch.jsx
+++ frontend/src/pages/CreateDespatch.jsx
@@ -238,37 +238,10 @@ export default function CreateDespatch() {
             </Button>
 
             <Button
-              onClick={handleSendEmailConfirm}
-              className="w-full bg-[#6EE7B7] hover:bg-[#34D399] text-black flex justify-center items-center gap-2"
-              disabled={loading}
+              onClick={handleSendEmailClick}
+              className="w-full mb-4 bg-[#9F91E9] hover:bg-[#8F81D9]"
             >
-              {loading ? (
-                <>
-                  <svg
-                    className="animate-spin h-5 w-5 text-black"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-                    ></path>
-                  </svg>
-                  Sending...
-                </>
-              ) : (
-                "Send Email"
-              )}
+              Send Email
             </Button>
 
             <Button
